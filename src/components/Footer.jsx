41e50e71d65d7b20bbdee5acc06aa2d0d5decc51
ellisoalgo <ellisjwhitney@gmail.com>
2024-07-11 10:33:07 +0200

    new version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer">
       <Link to="https://github.com/Adalab/modulo-3-evaluacion-final-ellisoalgo" target='_blank' className='footer_link'>Check out my code</Link>
       <Link to="https://www.linkedin.com/in/elliswhitney/" target='_blank' className='footer_link'>Pay me to make stuff</Link>
-      <Link to="mailto:ellisjwhitney@gmail.com" target='_blank' className='footer_link'>Slide into my DMs</Link>
+      <Link to="mailto:hey@elliswhitney.com" target='_blank' className='footer_link'>Slide into my DMs</Link>
     </footer>
   )
 }
